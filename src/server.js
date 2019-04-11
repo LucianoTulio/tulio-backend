4de36ec93716073674c9330df75e9719302c7250
Luciano Tulio <luciano.tulio@me.com>
2019-04-11 05:47:41 -0300

    correcoes

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use((req,res,next)=> {
 })
 
 //midleware responsavel em gerenciar o cors da aplicacao crossdomain
-app.use(cors())
+app.use(cors());
 
 /*Midleware que ajuda o servidor a 
 *  entender requisicoes no formato JSON
